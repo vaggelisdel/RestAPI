932dcb000b8b0a293342b8fdd50e560dcbd2259e
vaggelisdel <delliosvaggelis@gmail.com>
2019-04-05 01:40:49 +0300

    Get & Post requests

---
+++ routes/id_cards.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var router = express.Router();
+const MongoClient = require('mongodb').MongoClient;
+const database = require('../database');
+
+const db_table = "id_cards";
+
+
+router.get("/", (request, response) => {
+    response.render("id_cards", {title: "ID Cards"});
+});
+
+router.get("/getid_cards", (request, response) => {
+    MongoClient.connect(database.db_url, {useNewUrlParser: true}, function (err, client) {
+        const collection = client.db(database.db_name).collection(db_table);
+        collection.find({}).toArray((error, result) => {
+            if (error) {
+                return response.status(500).send(error);
+            }
+            response.send(result);
+        });
+    });
+});
+
+router.post("/", (request, response) => {
+    MongoClient.connect(database.db_url, {useNewUrlParser: true}, function (err, client) {
+        const collection = client.db(database.db_name).collection(db_table);
+        var card = {
+            username: request.body.username,
+            card_id: request.body.card_id,
+            name: request.body.name,
+            surname: request.body.surname,
+            fathersname: request.body.fathersname,
+            birthdate: request.body.birthdate
+        };
+        collection.insertOne(card, (error, result) => {
+            if (error) {
+                return response.status(500).send(error);
+            }
+            // response.send(result.result);
+            response.redirect("/id_cards");
+        });
+    });
+});
+
+module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var router = express.Router();
+const MongoClient = require('mongodb').MongoClient;
+const database = require('../database');
+
+const db_table = "users";
+
+
+router.get("/", (request, response) => {
+    response.render("users", {title: "Users"});
+});
+
+router.get("/getusers", (request, response) => {
+    MongoClient.connect(database.db_url, {useNewUrlParser: true}, function (err, client) {
+        const collection = client.db(database.db_name).collection(db_table);
+        collection.find({}).toArray((error, result) => {
+            if (error) {
+                return response.status(500).send(error);
+            }
+            response.send(result);
+        });
+    });
+});
+
+router.post("/", (request, response) => {
+    MongoClient.connect(database.db_url, {useNewUrlParser: true}, function (err, client) {
+        const collection = client.db(database.db_name).collection(db_table);
+        var newuser = {
+            username: request.body.username,
+            password: request.body.password,
+            email: request.body.email
+        };
+        collection.insertOne(newuser, (error, result) => {
+            if (error) {
+                return response.status(500).send(error);
+            }
+            // response.send(result.result);
+            response.redirect("/users");
+        });
+    });
+});
+
+module.exports = router;
